8fad30dabd127d86fa45bd53f5b59003ea5eb19d
Zac Richards <107489668+Zacgoose@users.noreply.github.com>
2025-11-07 01:53:58 +0800

    Update Invoke-ExecStoreBeaconProducts.ps1

--- lightingdesign-api/Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Designer/Products/Invoke-ExecStoreBeaconProducts.ps1
+++ lightingdesign-api/Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Designer/Products/Invoke-ExecStoreBeaconProducts.ps1
@@ -49,7 +49,7 @@ Function Invoke-ExecStoreBeaconProducts {
                 RowKey       = [guid]::NewGuid().ToString()
                 ProductJson  = [string]$Product
             }
-            Add-CIPPAzDataTableEntity -Context $Table.Context -Entity $entity -Force -CreateTableIfNotExists
+            Add-CIPPAzDataTableEntity -Context $Table.Context -Entity $entity -CreateTableIfNotExists
         }
 
         $StatusCode = [HttpStatusCode]::Created
